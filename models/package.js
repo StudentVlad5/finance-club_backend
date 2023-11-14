5db1ba0b5ae3e50d05f68c73af534b0ecbab2977
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-11-14 06:57:08 +0200

    add db package

--- models/package.js
+++ models/package.js
@@ -4,89 +4,24 @@ require("mongoose-type-email");
 require("mongoose-type-url");
 
 const packagesValidationSchema = Joi.object({
-  // user_id: Joi.string().min(3).max(32).required(),
-  // basket: Joi.array().required(),
-  // totalAmount: Joi.string().min(1).max(32).required(),
-  // totalDiscount: Joi.string().min(1).max(32).required(),
-  // totalPayment: Joi.string().min(1).max(32).required(),
-  // delivery: Joi.array().required(),
-  // metodPayment: Joi.array().required(),
+  title: Joi.string().min(3).max(32).required(),
+  price: Joi.string().min(3).max(32).required(),
+  content: Joi.string().min(1).max(999).required(),
 });
 
 const packagesSchema = new mongoose.Schema(
   {
-    user_id: {
+    title: {
       type: String,
-      default: "not avtorization",
+      required: [true, "Set title of the package"],
     },
-    basket: {
-      type: Object,
-      required: [true, "Set basket order"],
-      default: {},
-    },
-    totalAmount: {
-      type: Number,
-      required: [true, "Set totalAmount"],
-    },
-    totalDiscount: {
-      type: Number,
-      required: [true, "Set totalDiscount"],
-    },
-    totalPayment: {
-      type: Number,
-      required: [true, "Set totalPayment"],
-    },
-    currency: {
+    price: {
       type: String,
-      required: [true, "Set totalPayment"],
+      required: [true, "Set price of the package"],
     },
-    deliveryOrder: {
-      type: Object,
-      required: [true, "Set delivery"],
-      default: {},
-    },
-    selectedPaymentOption: {
+    content: {
       type: String,
-      required: [true, "Set methodPayment"],
-    },
-    name: {
-      type: String,
-      required: [true, "Set userName"],
-    },
-    company: {
-      type: String,
-      // required: [true, "Set company"],
-    },
-    city: {
-      type: String,
-      required: [true, "Set city"],
-    },
-    address1: {
-      type: String,
-      required: [true, "Set address1"],
-    },
-    address2: {
-      type: String,
-      // required: [true, "Set address2"],
-    },
-    state: {
-      type: String,
-      required: [true, "Set state"],
-    },
-    zipCode: {
-      type: String,
-      required: [true, "Set zipCode"],
-    },
-    phone: {
-      type: String,
-      required: [true, "Set phone"],
-    },
-    email: {
-      type: String,
-      required: [true, "Set email"],
-    },
-    comments: {
-      type: String,
+      required: [true, "Set content of the the package"],
     },
   },
   {
