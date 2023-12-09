cca58645efd4adf1970f08cb6607e3b3bf370be7
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-12-09 19:12:07 +0200

    add corsOptions

--- app.js
+++ app.js
@@ -14,7 +14,19 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors());
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+const corsOptions = {
+origin: ['https://finance-club-amber.vercel.app'],
+allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
+credentials: true,
+enablePreflight: true
+}
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions))
+
+// app.use(cors());
+// app.options('*', cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
